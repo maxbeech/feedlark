73e74c70ee2cda423d5546e92f3ec5d9bdf0ad11
maxbeech <maxedbeech@gmail.com>
2026-07-19 22:18:54 +0200

    feat(blog): add 6 posts covering remaining non-cannibalizing keyword gaps

--- src/lib/content/blog.ts
+++ src/lib/content/blog.ts
@@ -16,6 +16,7 @@ import { BLOG_FEEDBACK_EXTRA } from "./blog-feedback-extra";
 import { BLOG_REVIEWS_EXTRA } from "./blog-reviews-extra";
 import { BLOG_NEWS_EXTRA } from "./blog-news-extra";
 import { BLOG_CLIENT_FEEDBACK } from "./blog-client-feedback";
+import { BLOG_CONTENT_GAP_FILLERS } from "./blog-content-gap-fillers";
 
 export const BLOG_POSTS: BlogPost[] = [
   ...BLOG_FEEDBACK_BASICS,
@@ -31,6 +32,7 @@ export const BLOG_POSTS: BlogPost[] = [
   ...BLOG_REVIEWS_EXTRA,
   ...BLOG_NEWS_EXTRA,
   ...BLOG_CLIENT_FEEDBACK,
+  ...BLOG_CONTENT_GAP_FILLERS,
 ];
 
 export function getPost(slug: string): BlogPost | undefined {
